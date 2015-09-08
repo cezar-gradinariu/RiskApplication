bb511b8ddf5f0087e61cd66aa0f84c15c90339d7
cezar-gradinariu <cezar.gradinariu@gmail.com>
2015-09-09 03:26:06 +1000

    Added the portal
    At this moment it is not linked to the lower layers. It does not call
    risk service for information and the model is still using the classes
    from the domain.

---
+++ RiskApplication/Portal/Controllers/RiskController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Domain.Models;
+
+namespace Portal.Controllers
+{
+    public class RiskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new List<CustomerStatics>());
+        }
+
+        [Route("/Risk/GetSettledBet/{id}")]
+        public ActionResult GetSettledBet(int id)
+        {
+            return PartialView("SettledBets", new List<SettledBet>());
+        }
+    }
+}
---
+++ RiskApplication/Portal/Scripts/Custom/Risk.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#statistics > tbody > tr').on('click', function () {
+        $('#statistics > tbody > tr').removeClass('selectedRow');
+        $(this).addClass('selectedRow');
+        $('#settledBets').load('/Risk/GetSettledBet/' + $(this).find('td').first().text());
+    });
+});
